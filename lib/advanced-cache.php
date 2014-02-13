dcef26a0a53597b21ba628de626e99557e7b9907
GatorDog <GatorDog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-13 04:44:24 +0000

    tagging version 1.42
    git-svn-id: https://plugins.svn.wordpress.org/gator-cache/tags/1.42@856921 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/advanced-cache.php
+++ lib/advanced-cache.php
@@ -11,10 +11,15 @@ if(!defined('GC_CHK_USER') && $config->get('skip_user')){
             define('GC_CHK_USER', true);
             return;
         }
+        if(0 === strpos($cookies[$xx], 'comment_author')){
+            return;
+        }
     }
 }
 $request = GatorCache::getRequest();
-if('GET' !== $request->getMethod() || '' !== $request->getQueryString() || $request->isSecure()){//skip ssl for now
+if('GET' !== $request->getMethod() || '' !== $request->getQueryString() || ($request->isSecure() && $config->get('skip_ssl'))
+  || false === ($host = $config->get('host')) || $host !== $request->getHost()
+  || ($config->get('dir_slash') && '/' !== substr($request->getBasePath(), -1))){
     return;
 }
 if(false !== ($result = ($cache = GatorCache::getCache($opts = $config->toArray())->get($request->getBasePath(), $opts['group'])))){
@@ -24,5 +29,5 @@ if(false !== ($result = ($cache = GatorCache::getCache($opts = $config->toArray(
     if(!empty($opts['pingback'])){
         header('X-Pingback: ' . $opts['pingback']);
     }
-    die($result . ($opts['debug'] ? "\n<!-- Served by Advanced Cache -->\n" : ''));
+    die($result . ($opts['debug'] ? "\n<!-- Served by Advanced Cache " . $host . " -->\n" : ''));
 }
